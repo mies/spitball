b665e904165b92716c1eb4beff441432ba975b51
ericfode <ericfode@gmail.com>
2013-07-16 11:21:00 -0700

    Punt on timestamp

--- src/main/scala/services/Spitball.scala
+++ src/main/scala/services/Spitball.scala
@@ -3,33 +3,48 @@ package services
 import org.slf4j.LoggerFactory
 import com.heroku.logfmt.Logfmt
 import scala.collection.JavaConverters._
+import scala.annotation.tailrec
+import java.text.SimpleDateFormat
+import spray.json._
 
 object Spitball {
 
   private lazy val logger = LoggerFactory.getLogger(Spitball.getClass)
   private lazy val redisService = RedisService()
+  private val datep = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssZ")
+
+
+  case class LogLine(val line:String, val time:java.util.Date)
+  case class LogValue(val value:String, val time:java.util.Date)
 
   def get(requestId: String) = fromRedis(requestId)
 
   def drain(logs: String) {
     parse(logs).foreach { line =>
-      val parsedLine = Logfmt.parse(line.toCharArray).asScala.toMap.mapValues(new String(_))
+      val parsedLine = Logfmt.parse(line.line.toCharArray).asScala.toMap.mapValues(new String(_))
       forRequestId(parsedLine).map { entry =>
         val requestId = entry._1
         val pairs = entry._2.filterKeys(_.startsWith("measure."))
-        toRedis(requestId, pairs)
+        val jsonPairs =  pairs.map { kv =>
+          val key = kv._1
+          val value = kv._2
+          (key, LogValue(value,line.time).toJson.toString())
+        }
+        toRedis(requestId, jsonPairs)
       }
     }
   }
 
-  private def parse(in: String): Iterator[String] = {
-    def loop(unparsed: Iterator[Char], parsed: Iterator[String]): Iterator[String] = {
+  private def parse(in: String): Iterator[LogLine] = {
+    @tailrec
+    def loop(unparsed: Iterator[Char], parsed: Iterator[LogLine]): Iterator[LogLine] = {
       if (unparsed.isEmpty) parsed
       else {
         val (head, tail) = unparsed.span(_ != ' ')
         val length = head.mkString.toInt
         val chunk = tail.slice(1, 1 + length).mkString
-        loop(tail, parsed ++ Iterator(chunk))
+        val time = datep.parse(chunk.split(' ')(1))
+        loop(tail, parsed ++ Iterator(LogLine(chunk, time)))
       }
     }
     loop(in.toIterator, Iterator.empty)
